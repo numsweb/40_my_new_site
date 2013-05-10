14116b512e889c69f0e683a6af1400ff57609192
John Kropka <jkropka@mojoware.com>
2013-05-10 15:16:34 -0500

    initial release of rails 4.0 project

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,34 @@
+class HomeController < ApplicationController
+  layout "application"
+  def index
+  end
+  
+  def about
+    
+  end
+  
+  def support
+    
+  end
+  
+  def clients
+    
+  end
+  
+  
+  def support
+    
+  end
+  
+  def design
+    
+  end
+  
+  def success
+    
+  end
+  
+  def services
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp4::Application.config.secret_token = '628cc031040cad12452141d764a4d47d5139cdf945bbc1c55b60d19341331795eb5eecc7fb1f2cfa650f28718bfa7cdc9afa374340e492f362af5e829e33dbc0'
